10b0ecb516887bb39310906c588d3e2684f739c9
Sasha Jaroshevskii 🦄 <72662383+jaroshevskii@users.noreply.github.com>
2025-08-21 23:57:22 +0300

    Initial Commit

---
+++ TCAPrimeNumberPlus/TCAPrimeNumberPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPrimeNumberPlusApp.swift
+//  TCAPrimeNumberPlus
+//
+//  Created by Sasha Jaroshevskii on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAPrimeNumberPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
